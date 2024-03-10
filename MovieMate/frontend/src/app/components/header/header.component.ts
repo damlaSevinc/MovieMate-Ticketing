e4450ce71a323c8dc92c39dcf214c6b032f0217c
Damla Sevinc Tokdemir <sevincdamla@gmail.com>
2024-03-10 20:12:23 +0100

    added signOut option according to loggedInUser

--- MovieMate/frontend/src/app/components/header/header.component.ts
+++ MovieMate/frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  loggedInUser: User | null = null;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+      this.authService.getLoggedInUserOb().subscribe((user) => {
+        this.loggedInUser = user;
+      })
+  }
+
+  signOut(){
+    this.authService.clearToken();
+    this.router.navigate(['/home']);
+  }
 }

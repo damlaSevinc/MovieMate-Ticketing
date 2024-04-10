22fe1c4861d6e873eeafd84863aee093f1057f93
Damla Sevinc Tokdemir <sevincdamla@gmail.com>
2024-04-10 23:11:27 +0200

    add a form to edit the existing data on profile page on frontend

--- MovieMate/frontend/src/app/pages/profile/personal-info/personal-info.component.ts
+++ MovieMate/frontend/src/app/pages/profile/personal-info/personal-info.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-personal-info',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class PersonalInfoComponent {
 
+  loggedInUser: User | null = null;
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.getLoggedInUserOb().subscribe((User) => {
+      this.loggedInUser = User;
+    });
+    if (this.loggedInUser) {
+      axios.get(`/profile/${this.loggedInUser.id}`)
+        .then(response => {
+          console.log("Profile of the user:", response.data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }
+
+  submitForm() {
+    const formData = {
+      firstName: this.loggedInUser?.firstName,
+      lastName: this.loggedInUser?.lastName,
+      email: this.loggedInUser?.email
+    }
+    console.log("formdata: ", formData);
+    axios.put(`/profile/${this.loggedInUser!.id}`, formData)
+      .then(response => {
+        console.log("put request worked")
+      })
+      .catch(error => {
+      console.error(error);
+      })
+  }
+
 }
+

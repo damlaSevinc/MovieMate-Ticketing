a6dd583aeadc1b0e7b744e5024706a5bf573c604
Damla Sevinc Tokdemir <sevincdamla@gmail.com>
2024-04-19 16:19:01 +0200

    add basic seat selection before checkout

--- MovieMate/frontend/src/app/pages/seat-selection/seat-selection.component.ts
+++ MovieMate/frontend/src/app/pages/seat-selection/seat-selection.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+interface Seat {
+  number: string;
+  selected: boolean;
+  available: boolean;
+}
+
+interface Row {
+  number: number;
+  seats: Seat[];
+}
 @Component({
   selector: 'app-seat-selection',
   templateUrl: './seat-selection.component.html',
@@ -11,6 +21,7 @@ export class SeatSelectionComponent implements OnInit {
   movieId = 0;
   showtimeId = 0;
   selectedDate = '';
+  rows: Row[] = [];
 
   constructor(
     private activatedRouter: ActivatedRoute,
@@ -27,6 +38,7 @@ export class SeatSelectionComponent implements OnInit {
     console.log('movieId: ' + this.movieId);
     console.log('showtimeId: ' + this.showtimeId);
     console.log('selectedDate: ' + this.selectedDate);
+    this.initializeSeats();
   }
 
   backToShowtimes() {
@@ -38,13 +50,33 @@ export class SeatSelectionComponent implements OnInit {
     this.router.navigate(['/home'])
   }
 
+  initializeSeats(){
+    const numRows = 5;
+    const numSeatsPerRow = 10;
+
+    for (let i = 0; i < numRows; i++){
+      const row: Row = { number: i, seats: [] };
+      for (let j = 0; j < numSeatsPerRow; j++) {
+        row.seats.push({ number: `${j}`, selected: false, available: true})
+      }
+      this.rows.push(row);
+    }
+
+  }
+
+  toggleSeatSelection(seat: Seat): void {
+    if (seat.available) {
+      seat.selected = !seat.selected;
+    }
+  }
+
   goToCheckout() {
     this.router.navigate(['/checkout'],
       {
         queryParams: {
           movieId: this.movieId,
           showtimeId: this.showtimeId,
-          selectedDate: this.selectedDate
+          selectedDate: this.selectedDate,
         }
       })
   }

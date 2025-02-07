ec7010c325dd37c20d6ce6d340ace26c3e78dd74
damlaSevinc <sevincdamla@gmail.com>
2025-02-07 22:04:13 +0100

    Lint: Fix error

--- backend/src/main/java/com/moviemate/repositories/MovieRepository.java
+++ backend/src/main/java/com/moviemate/repositories/MovieRepository.java
@@ -3,12 +3,15 @@ package com.moviemate.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.moviemate.entities.Movie;
 
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>{
 
+    @NonNull
     List<Movie> findAll();
 }

2ae4873e401011d9e38e82d356383312d3362177
damlaSevinc <sevincdamla@gmail.com>
2025-02-12 23:26:40 +0100

    Remove unnecessary builtin method in the repository

--- backend/src/main/java/com/moviemate/repositories/ShowtimeRepository.java
+++ backend/src/main/java/com/moviemate/repositories/ShowtimeRepository.java
@@ -1,7 +1,6 @@
 package com.moviemate.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,5 +12,4 @@ public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
 
     List<Showtime> findByMovieId(Long movieId);
 
-    Optional<Showtime> findById(Long showtimeId);
 }

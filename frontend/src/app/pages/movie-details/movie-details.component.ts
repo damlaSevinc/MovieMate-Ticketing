5ee2d2b9f92d0acb52ae8a056158748f33dc5854
damlaSevinc <sevincdamla@gmail.com>
2025-07-15 23:28:07 +0200

    Fix: ESLint warnings

--- frontend/src/app/pages/movie-details/movie-details.component.ts
+++ frontend/src/app/pages/movie-details/movie-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import axios from 'axios';
+import { NgToastService } from 'ng-angular-popup';
 import { Movie } from 'src/app/models/movie';
 
 @Component({
@@ -14,29 +15,29 @@ export class MovieDetailsComponent implements OnInit {
   movieId = 0;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private toast: NgToastService
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe(params => {
-      this.movieId = +params['movieId'];
-      // added + operator to coerce the number type, even if JS mostly converts it to number implicitly
-      this.getMovieDetails();
+      const id = Number(params['movieId'] ?? 0);
+      this.movieId = isFinite(id) && id > 0 ? id : 0;
+      void this.getMovieDetails();
     })
   }
 
-  getMovieDetails(): void {
-    axios.get(`/movies/${this.movieId}`)
-      .then(response => {
-        this.movie = response.data;
-      })
-      .catch(error => {
-        console.error(error);
-      })
+  async getMovieDetails() {
+    try {
+      const response = await axios.get(`/movies/${String(this.movieId)}`);
+      this.movie = response.data as Movie;
+    } catch (error) {
+      console.error(error);
+      this.toast.error({detail: 'ERROR', summary: 'Failed to fetch movie details.', duration: 4000});
+    }
   }
 
-  getShowtimes(): void {
-    this.router.navigate(['/showtimes'],
-    { queryParams: { movieId: this.movieId }})
+  getShowtimes() {
+    void this.router.navigate(['/showtimes'], { queryParams: { movieId: this.movieId }});
   }
 }
